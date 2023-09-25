e60da41bcfd03f3cb57d8a6af8ee5d361f97024e
송규진 <songu9610@gmail.com>
2023-09-25 10:11:52 +0900

    브랜치 생성

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.mymedia"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.mymedia"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    //gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    //glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    //retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+    // viewmodel 및 lifecycle
+    implementation("com.android.identity:identity-credential:20230420")
+    val lifecycle_version = "2.5.1"
+    val arch_version = "2.1.0"
+
+    //activityViewmodel
+    implementation("androidx.activity:activity-ktx:1.5.7")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/example/mymedia/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mymedia.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymedia.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
