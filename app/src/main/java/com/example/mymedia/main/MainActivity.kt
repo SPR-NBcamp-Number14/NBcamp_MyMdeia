bd256d5f6135ebe95be0bceee8a0bff39db4a171
송규진 <songu9610@gmail.com>
2023-09-25 12:11:17 +0900

    [FEAT] : ⚙️: 뷰 바인딩 설정

--- app/src/main/java/com/example/mymedia/main/MainActivity.kt
+++ app/src/main/java/com/example/mymedia/main/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.mymedia.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import com.example.mymedia.R
 import com.example.mymedia.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: MainActivityBinding
+    private lateinit var binding:MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initView()
     }
 
+    private fun initView() = with(binding){
+
+    }
 }
