c972a36657a580c443cb8e638dda141d68d6964a
송규진 <songu9610@gmail.com>
2023-09-25 10:35:34 +0900

    [FEAT] : ⚙️: 뷰 바인딩 설정

--- app/src/main/java/com/example/mymedia/main/MainActivity.kt
+++ app/src/main/java/com/example/mymedia/main/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.mymedia.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.mymedia.R
+import com.example.mymedia.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
--- app/src/main/java/com/example/mymedia/my_video/MyVideoFragment.kt
+++ app/src/main/java/com/example/mymedia/my_video/MyVideoFragment.kt
@@ -5,22 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mymedia.R
+import com.example.mymedia.databinding.MyVideoFragmentBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyVideoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class MyVideoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding : MyVideoFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,19 +33,12 @@ class MyVideoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.my_video_fragment, container, false)
+       _binding = MyVideoFragmentBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyVideoFragment.
-         */
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
